e0c58b6a90e7592ac7a26a351a33f06e6a321c06
Moises P. Sena <moisespsena@gmail.com>
2018-09-25 11:55:56 -0300

    Implement fake db cache

---
+++ fake.go
@@ -0,0 +1,28 @@
+package aorm
+
+import "sync"
+
+var fakeDBMap sync.Map
+
+// CreateFakeDB create a new fake db with dialect
+func CreateFakeDB(dialect string) (db *DB) {
+	db = &DB{
+		values:  map[string]interface{}{},
+		dialect: newDialect(dialect, nil),
+	}
+	db.parent = db
+	fakeDBMap.Store(dialect, db)
+	return
+}
+
+// FakeDB get or create a new fake db with dialect
+func FakeDB(dialect string) (db *DB) {
+	dbi, ok := fakeDBMap.Load(dialect)
+	if ok {
+		db = dbi.(*DB)
+	} else {
+		db = CreateFakeDB(dialect)
+		fakeDBMap.Store(dialect, db)
+	}
+	return
+}
